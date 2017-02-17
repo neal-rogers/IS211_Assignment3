dd2cefe7297778fcec02ea33250e0c7f7ac228d9
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-16 19:23:00 -0500

    Removed extra tls lines.

--- assignment3.py
+++ assignment3.py
@@ -23,58 +23,54 @@ def downloadData(url):
     data = response.read()
     return data
 
+def processData(response_data):
+    """
+    Args:
+        response_data (str): Contents of data from downloadData function.
+    Returns:
+        myresult_dict (dict): Dictionary file containing formatted records.
+    Example:
+        >> processData(csvdata)
+        >>
+    """
+    # Creates dict, 'browsers', with default values.
+    browsers = {'Firefox':0,
+                'Chrome':0,
+                'Internet Explorer':0,
+                'Safari':0}
+    # Creates dict, 'imghits', with default values.
+    imghits = {'img':0,
+               'rowcount':0}
 
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        def processData(response_data):
-            """
-            Args:
-                response_data (str): Contents of data from downloadData function.
-            Returns:
-                myresult_dict (dict): Dictionary file containing formatted records.
-            Example:
-                >> processData(csvdata)
-                >>
-            """
-            # Creates dict, 'browsers', with default values.
-            browsers = {'Firefox':0,
-                        'Chrome':0,
-                        'Internet Explorer':0,
-                        'Safari':0}
-            # Creates dict, 'imghits', with default values.
-            imghits = {'img':0,
-                       'rowcount':0}
+    # Creates csv reader object, 'reader'.
+    #reader = csv.reader(response_data.split("\n"))
+    # For each line in 'response_list' within specified range...
+    for row in csv.reader(response_data):
+    #for row in reader:
+        # Count total number of page hits while in loop.
+        imghits['rowcount'] += 1
+        if re.search(r"(?i)(jpg|jpeg)|gif|png", row[0]):
+            imghits['img'] += 1
+        if re.search(r"Firefox", row[2]):
+            browsers['Firefox'] += 1
+        elif re.search(r"Chrome", row[2]):
+            browsers['Chrome'] += 1
+        elif re.search(r"MSIE", row[2]):
+            browsers['Internet Explorer'] += 1
+        elif re.search(r'Safari', row[2]):
+            browsers['Safari'] += 1
 
-            # Creates csv reader object, 'reader'.
-            reader = csv.reader(response_data.split("\n"))
-            # For each line in 'response_list' within specified range...
-            for row in reader:
-                # Count total number of page hits while in loop.
-                rowcount += 1
-                if re.search():
-                    imghits[img] += 1
-                if re.search():
-                    browsers['Firefox'] += 1
-                elif re.search():
-                    browsers['Chrome'] += 1
-                elif re.search():
-                    browsers['Internet Explorer'] += 1
-                elif re.search():
-                    browsers['Safari'] += 1
-
-            return myresult_dict
-            print myresult_dict
-            # Determine the percentage imghits and print result.
-            imgcalc = (float(imghits['img'])) / (float(imghits['rowcount']))*100
-            print "Image requests account for {}% of all requests.".format(imgcalc)
-            # Determine most popular browser and print result.
-            maximum = max(browsers, key=browsers.get)
-            print "The most popular browser is {}.".format(maximum)
+    # Determine the percentage imghits and print result.
+    imgcalc = (float(imghits['img'])) / (float(imghits['rowcount'])) * 100
+    print "Image requests account for {}% of all requests.".format(imgcalc)
+    # Determine most popular browser and print result.
+    maximum = max(browsers, key=browsers.get)
+    print "The most popular browser is {}.".format(maximum)
 
 def main():
     url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
     csvdata = downloadData(url)
-    result = processData(csvdata)
+    processData(csvdata)
 """
 def main():
     parser = argparse.ArgumentParser()
