19a2bf1208fb763a562c2af70d0a168e0bc54c6c
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-19 19:42:11 -0500

    For some unknown reason the program kept throwing exceptions about the main() block. I couldn't find any source for the error but after cuttting and pasting each line into a blank file, testing successfully, deleted all text from original file, and pasting back the program works. I don't understand how it's working again or why it stopped to begin with but it's working.

--- assignment3.py
+++ assignment3.py
@@ -5,10 +5,10 @@
 """
 
 
-import csv, urllib2, re, argparse
+import argparse, urllib2, csv, re
 
 
-def downloadData(url):
+def download_data(url):
     """
     Args:
         url (str): URL for fetching of data.
@@ -18,11 +18,8 @@ def downloadData(url):
         >> downloadData(url)
         >>
     """
-    # Generates HTTP request for the passed 'url'; returns/stores HTTP response.
-    response = urllib2.urlopen(url)
-    # Sets 'data' to the contents of 'response'.
-    data = response.read()
-    return data
+    data_content = urllib2.urlopen(url)
+    return data_content
 
 
 def process_data(response_data):
@@ -33,57 +30,48 @@ def process_data(response_data):
         imgcalc (float): Floating point value for percentage of image hits.
         maximum (int): Highest integer value for browsers.
     Example:
-        >> processData(csvdata)
+        >> process_data(csvdata)
         >>
     """
-    # Creates dict, 'browsers', with default values.
     browsers = {'Firefox': 0,
                 'Chrome': 0,
                 'Internet Explorer': 0,
                 'Safari': 0}
-    # Creates dict, 'imghits', with default values.
+
     imghits = {'img': 0,
                'rowcount': 0}
 
-    # Creates csv reader object, 'reader'.
-    # For each line in 'response_data' within specified range...
     for row in csv.reader(response_data):
-        # Count total number of page hits while in loop.
         imghits['rowcount'] += 1
         if re.search(r"(?i)(jpg|jpeg)|gif|png", row[0]):
             imghits['img'] += 1
-        if re.search("firefox", row[2]):
-            browsers['Firefox'] += 1
+        if re.search("MSIE", row[2]):
+            browsers['Internet Explorer'] += 1
         elif re.search("Chrome", row[2]):
             browsers['Chrome'] += 1
-        elif re.search("MSIE", row[2]):
-            browsers['Internet Explorer'] += 1
-        elif re.search('Safari', row[2]):
+        elif re.search("firefox", row[2], re.I):
+            browsers['Firefox'] += 1
+        elif re.search("Safari", row[2]) and not re.search("Chrome", row[2]):
             browsers['Safari'] += 1
 
-    # Determine the percentage imghits and print result.
     imgcalc = (float(imghits['img'])) / (float(imghits['rowcount'])) * 100
     print "Image requests account for {}% of all requests.".format(imgcalc)
 
-    # Determine most popular browser and print result.
     maximum = max(browsers, key=browsers.get)
     print "The most popular browser is {}.".format(maximum)
 
 
 def main():
-    url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
-    csvdata = downloadData(url)
-    process_data(csvdata)
-    """parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('url', help='Enter the data url')
     args = parser.parse_args()
+
     if args.url:
-        url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
-        csvdata = downloadData(url)
-        processData(csvdata)
+        #url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
+        csvdata = download_data(args.url)
+        process_data(csvdata)
     else:
         print 'error'
-    """
 
 if __name__ == '__main__':
     main()
